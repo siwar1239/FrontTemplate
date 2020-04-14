678d783589b3e68fa1f6c680b8fe389f85dda9ac
siwar zayani <siwar.zayani@softtodo.com>
2020-04-14 17:52:18 +0200

    background footer

--- Templates/Partials/footer.php
+++ Templates/Partials/footer.php
@@ -1,19 +1,7 @@
 <footer id="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-12 logo-footer-left">
-                    <figure>
-                        <img src="../../Img/logo.svg" alt="log footer">
-                    </figure>
-                </div>
-                <div class="col-md-3 col-12 informations-right">
-                    <span>Adresse: Route de tunis KM8</span>
-                    <span>Tel: <a href="tel:+216 23975444">+216 23975444</a></span>
-                    <span>E-Mail: <a href="mailto:siwar.zayani@softtodo.com">siwar.zayani@softtodo.com</a></span>
-                </div>
-            </div>
-            <div class="copyright-footer">
-                Copyright © 2020
+        <div class="container-fluid">
+            <div class="container">
+                fdgsd
             </div>
         </div>
 </footer>
