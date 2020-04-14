d24e73820ff956a1021f625f07b85f442bd5233e
siwar zayani <siwar.zayani@softtodo.com>
2020-04-14 17:26:14 +0200

    add request informations text of home page

---
+++ Templates/ContentElements/text-request-info.php
@@ -0,0 +1,31 @@
+<section id="blocks">
+    <div class="container">
+        <div class="row w-100">
+            <div class="col-lg-3 col-sm-6 col-xs-4">
+                <img src="../../Img/bgm_icone.jpg" class="py-3" >
+            </div>
+            <div class="col-lg-3 col-sm-6 col-xs-4">
+                <img src="../../Img/bm_icone.jpg" class="py-3" >
+            </div>
+            <div class="col-lg-3 col-sm-6 col-xs-4">
+                <img src="../../Img/ch_icone.jpg" class="py-3" >
+            </div>
+            <div class="col-lg-3 col-sm-6 col-xs-4">
+                <img src="../../Img/em_icone.jpg" class="py-3" >
+            </div>
+        </div>
+    </div>
+</section>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-10 col-md-offset-2">
+            <span>WZAT – das Ärzteteam an Ihrer Seite </span>
+            <p class="text-body">
+                Als Patient mit lebensbedrohlicher Krankheit, die jedoch durch ein regelmäßiges und
+                zeitnahes Monitoring gut zu kontrollieren ist, ist das telemedizinische Angebot der WZAT
+                ein wesentlicher Schritt in ein selbstbestimmtes Leben zu Hause.
+            </p>
+
+        </div>
+    </div>
+</div>
