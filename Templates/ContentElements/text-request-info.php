5b9ef6f3591cb847fa4e5fd51cfbb46904e82af9
siwar zayani <siwar.zayani@softtodo.com>
2020-04-14 17:43:47 +0200

    edit footer background

--- Templates/ContentElements/text-request-info.php
+++ Templates/ContentElements/text-request-info.php
@@ -1,7 +1,7 @@
 <section id="request-info">
     <div class="container">
         <div class="row">
-            <div class="col-sm-10 col-md-offset-2">
+            <div class="col-sm-10">
                 <span class="title-body">WZAT – das Ärzteteam an Ihrer Seite </span>
                 <p class="text-body">
                     Als Patient mit lebensbedrohlicher Krankheit, die jedoch durch ein regelmäßiges und
