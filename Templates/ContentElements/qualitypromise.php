2b4103389eed39e597eb4a207818017f9a408456
siwar zayani <siwar.zayani@softtodo.com>
2020-04-15 12:09:25 +0200

    edit style qualitypromise

--- Templates/ContentElements/qualitypromise.php
+++ Templates/ContentElements/qualitypromise.php
@@ -1,9 +1,17 @@
 <section id="platzhalter">
    <div class="container">
-       <div class="title1">Persönlich für Sie da: +49 2306 7655-0 Kopie</div>
-       <div class="title2">Ihr Ärzteteam von WZAT</div>
-       <div class="image">
-           <img src="../../Img/platzhalter.jpg" class="py-3" >
+       <div class="title2">Unser Qualitätsversprechen</div>
+       <div class="row w-100">
+           <div class="col-lg-4 col-md-4 col-sm12 col-12 block-quality">
+               <div class="title-block">Datenschutz</div>
+           </div>
+           <div class="col-lg-4 col-md-4 col-sm12 col-12">
+               <div class="title-block">Qualitätskontrolle</div>
+           </div>
+           <div class="col-lg-4 col-md-4 col-sm12 col-12">
+               <div class="title-block">Technik</div>
+           </div>
+
        </div>
    </div>
 </section>
