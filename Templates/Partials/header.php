b7647ea8c88bc92012d35e46f9f9d8bbd95f4d93
siwar zayani <siwar.zayani@softtodo.com>
2020-04-15 17:33:34 +0200

    zd

--- Templates/Partials/header.php
+++ Templates/Partials/header.php
@@ -4,41 +4,18 @@
             aaaaaaaaaaa
         </div>
     </div>
+
+
     <div class="container">
         <div class="row">
-            <div class="col-sm-3 col-md-3 col-lg-3 logo">
-                <a href="home.php" title="logo">
-                    <img src="../../Img/logo_header.jpg" class="py-3" >
+            <div class="col-lg-3 col-md-3 col-sm-3 logo">
+                <a href="home.php" class="logo-a" title="logo">
+                        <img src="../../Img/logo_header.jpg" class="py-3" >
                 </a>
             </div>
-            <div class="col-sm-9 col-md-9  col-lg-9 main-menu-items">
-                <div class="mobile-nav-opener">
-                    <div id="nav-icon">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </div>
-                <div class="main-menu">
-                    <div class="main-menu-navs semi-white-bg">
-                        <div class="main-nav">
-                            <nav id="main-navigation">
-                                <ul class="first-level-menu">
-
-                                    <li class=""><a href="#">Unsere Versorgungsprogramme</a></li>
-                                    <li class=""><a href="#">Anwenderberichte</a></li>
-                                    <li class=""><a href="#">Über das WZAT</a></li>
-                                    <li class=""><a href="#">Kontakt</a></li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
+            <div class="col-lg-9 col-md-9 col-sm-9 menu">
+                <div class="menu-nav"><h2>Menu</h2></div>
             </div>
-
-
-
         </div>
     </div>
 </header>
