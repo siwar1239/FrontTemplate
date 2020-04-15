f368c3026a8a63ec2443166ee226dc36391a20c7
siwar zayani <siwar.zayani@softtodo.com>
2020-04-15 08:54:06 +0200

    comment favicons

--- Templates/Globals/head.php
+++ Templates/Globals/head.php
@@ -4,8 +4,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="format-detection" content="telephone=no">
-        <title>Lorem ipsum dolor</title>
-
+        <title>WZAT</title>
         <meta name="author" content="Author lorem ipsum">
         <meta name="description" content="Description lorem ipsum">
 
