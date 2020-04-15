ba45f4a76b667e67cff6cc62c72366ebc77191aa
siwar zayani <siwar.zayani@softtodo.com>
2020-04-15 14:17:15 +0200

    edit style of qualitypromise

--- Templates/ContentElements/qualitypromise.php
+++ Templates/ContentElements/qualitypromise.php
@@ -4,12 +4,46 @@
        <div class="row w-100">
            <div class="col-lg-4 col-md-4 col-sm12 col-12 block-quality">
                <div class="title-block">Datenschutz</div>
+               <ul>
+                   <li>Sensibler Umgang mit Ihren Daten<br>(ärztliche Schweigepflicht)</li>
+                   <li>Aktuelles Datenschutzkonzept</li>
+                   <li>ISO/IEC 27001 durch den Vertrags-<br>
+                       partner des WZAT: SVA, Operational<br>
+                       Services</li>
+               </ul>
            </div>
-           <div class="col-lg-4 col-md-4 col-sm12 col-12">
+           <div class="col-lg-4 col-md-4 col-sm12 col-12 block-quality">
                <div class="title-block">Qualitätskontrolle</div>
+               <ul>
+                       <li>Unabhängig geprüft: Das Qualitäts-
+                           management Zertifikat: Zertifiziert
+                           nach DIN ISO 9001:2015 der Tele-
+                           medizinischen Dienstleistung
+                           (Erneuerung ……)
+                       </li>
+                       <li>Nutzung einer eigenen zertifizierten
+                           Telemedizinsoftware (Medizinprodukt
+                           Klasse II a nach Anhang II) diese ent-
+                           spricht somit den höchsten Anforde-
+                           rungen an Datensicherheit, Daten-
+                           schutz und Softwareentwicklung.
+                       </li>
+
+               </ul>
            </div>
-           <div class="col-lg-4 col-md-4 col-sm12 col-12">
+           <div class="col-lg-4 col-md-4 col-sm12 col-12 block-quality">
                <div class="title-block">Technik</div>
+               <ul>
+                       <li>Bereitstellung der telemedizinischen
+                           Endgeräte–nach Indikation als
+                           Mietgeräte auf Monatsbasis: Inklusive
+                           Datenpauschale bzw. SIM Karte
+                       </li>
+                       <li>Technische Unterstützung von der
+                           Einweisung über laufende Fragen bis
+                           zum Geräteaustausch
+                       </li>
+               </ul>
            </div>
 
        </div>
