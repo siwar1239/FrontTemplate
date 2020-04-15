e8cab93a282cc9f2b3f94e584a69cee277e1087e
siwar zayani <siwar.zayani@softtodo.com>
2020-04-15 11:59:23 +0200

    add qualitypromise part

---
+++ Templates/ContentElements/qualitypromise.php
@@ -0,0 +1,9 @@
+<section id="platzhalter">
+   <div class="container">
+       <div class="title1">Persönlich für Sie da: +49 2306 7655-0 Kopie</div>
+       <div class="title2">Ihr Ärzteteam von WZAT</div>
+       <div class="image">
+           <img src="../../Img/platzhalter.jpg" class="py-3" >
+       </div>
+   </div>
+</section>
