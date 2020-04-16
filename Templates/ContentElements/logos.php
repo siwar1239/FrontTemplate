09ceabf83207342966c89dba9fdb4b5221652156
siwar zayani <siwar.zayani@softtodo.com>
2020-04-16 10:18:32 +0200

    little edit in logos file

--- Templates/ContentElements/logos.php
+++ Templates/ContentElements/logos.php
@@ -2,21 +2,21 @@
         <div class="container">
             <div class="row">
                 <div class="logos">
-                    <div class="col-lg-4 col-md-4 col-sm-4 col-12 logo">
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 logo">
                         <div class="logo-holder">
                             <figure>
                                 <img src="../Img/logo1.jpg" alt="Alt for image">
                             </figure>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-4 col-sm-4 col-12 logo">
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 logo">
                         <div class="logo-holder">
                             <figure>
                                 <img src="../Img/logo2.jpg" alt="Alt for image">
                             </figure>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-4 col-sm-4 col-12 logo">
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 logo">
                         <div class="logo-holder">
                             <figure>
                                 <img src="../Img/logo3.jpg" alt="Alt for image">
