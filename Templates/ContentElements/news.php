2991259910969db4b52c3218e456c3b272d65083
siwar zayani <siwar.zayani@softtodo.com>
2020-04-16 12:41:55 +0200

    news

--- Templates/ContentElements/news.php
+++ Templates/ContentElements/news.php
@@ -11,21 +11,9 @@ $btnName = isset($btnName) ? $btnName : '';
 ?>
 
 <div class="news <?php echo $class; ?> col-md-4 col-lg-4 col-xs-12">
-                        <div class="title_news">
-                            <?php echo $title; ?>
-                        </div>
+    <div class="title_news">
+        <?php echo $title; ?>
+    </div>
 
-                <div class="row infos-news">
-                    <div class="col-md-6 left-side">
-                        <img src="<?php echo $image; ?>" alt="">
-                    </div>
-                    <div class="col-md-6 right-side">
-                        <div class="description">
-                            <?php echo $description; ?>
-                        </div>
-                        <div class="more-news">
-                            <a href="<?php echo $btnLink; ?>" class=""><?php echo $btnName; ?></a>
-                        </div>
-                    </div>
-                </div>
 </div>
+
