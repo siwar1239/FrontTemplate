62cadf91c055f07db225beaaf1fac9f5eba7c18d
siwar zayani <siwar.zayani@softtodo.com>
2020-04-16 17:42:01 +0200

    edit container lg and md to 940px

--- Templates/home.php
+++ Templates/home.php
@@ -1,22 +1,9 @@
 <?php require_once '../Inc/functions.php'; //function import()?>
 <?php include 'Globals/head.php'; ?>
 <main>
-    <style>
-        @media (min-width: 1200px)
-        {
-            .container, .container-sm, .container-md, .container-lg, .container-xl {
-                max-width: 940px;
-            }
-        }
-    </style>
-
-
     <div class="lateral-menu">
             <a href="#" class="btn info_anfordern"></i><span>Information anfordern ></span></a>
     </div>
-
-
-
     <?php include 'Partials/header.php'; ?>
     <?php include 'ContentElements/slider.php'; ?>
     <?php include 'ContentElements/blocks.php'; ?>
@@ -33,8 +20,6 @@
     <?php include 'ContentElements/qualitypromise.php'; ?>
     <?php include 'ContentElements/team.php'; ?>
     <?php include 'ContentElements/logos.php'; ?>
-
-
 </main>
 <?php include 'Partials/footer.php'; ?>
 <?php include 'Globals/foot.php'; ?>
