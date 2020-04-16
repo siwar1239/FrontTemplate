74271f8aac735f870c1c28f59062e9bc96fbd491
siwar zayani <siwar.zayani@softtodo.com>
2020-04-16 09:29:37 +0200

    Edit footer part

--- Templates/Partials/footer.php
+++ Templates/Partials/footer.php
@@ -1,7 +1,44 @@
 <footer id="footer">
         <div class="container-fluid">
-            <div class="container">
-               Footer
+            <div class="container mt-3">
+               <div class="row w-100">
+                   <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 w-100 block_footer">
+                       <ul class="pl-0">
+                           <li>WZAT Westdeutsches Zentrum für</li>
+                           <li>angewandte Telemedizin GmbH</li>
+                           <li>Berliner Allee 44</li>
+                           <li>40212 Düsseldorf</li>
+                           <li>Tel 0221 416682-77</li>
+                           <li>Fax 0211 416682-8877</li>
+                           <li>info@wzat.de</li>
+                           <li>www.wzat.de</li>
+                       </ul>
+                   </div>
+                   <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 w-100 block_footer">
+                       <ul class="pl-0">
+                           <li>Medizinische Leitung WZAT</li>
+                           <li>Prof. Dr. med. Christian Perings</li>
+                           <li>Aldstadtstraße 22 b</li>
+                           <li>44534 Lünen</li>
+                           <li>info@wzat.de</li>
+                           <li>
+                               <img src="../Img/icone_footer1.png"/>
+                               <img src="../Img/icone_footer2.png"/>
+                           </li>
+                       </ul>
+
+                   </div>
+                   <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 w-100 block_footer">
+                       <ul class="pl-0">
+                           <li>Häufige Patienten-Fragen</li>
+                           <li>Was ist eigentlich Telemedizin?</li>
+                           <li>Wie funktioniert Telemonitoring?</li>
+                           <li>Wer sind meine Ansprechpartner?</li>
+                           <li>Was passiert mit meinen Daten?</li>
+                       </ul>
+                   </div>
+
+               </div>
             </div>
         </div>
 </footer>
