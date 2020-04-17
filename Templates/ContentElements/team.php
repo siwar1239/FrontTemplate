5da03c6c6180f2e82fd6bffb13bedb49fed82d5e
siwar zayani <siwar.zayani@softtodo.com>
2020-04-17 18:32:38 +0200

    add description to any image of slider team

--- Templates/ContentElements/team.php
+++ Templates/ContentElements/team.php
@@ -1,13 +1,57 @@
 <section id="team">
    <div class="container">
        <div class="title">Ihr WZAT Team</div>
-
        <div class="slider w-100">
-           <img src="../../Img/team/team1.png" />
-           <img src="../../Img/team/team2.png" />
-           <img src="../../Img/team/team3.png" />
-           <img src="../../Img/team/team4.png" />
-           <img src="../../Img/team/team1.png" />
+           <div >
+               <img src="../../Img/team/team1.png" />
+               <div class="content_slide mt-3">
+                   <span>Medizinische Leitung</span><br>
+                   <span class="color_blue">Prof. Dr. med.</span><br>
+                   <span class="color_blue">Christian Perings</span>
+               </div>
+
+           </div>
+           <div>
+               <img src="../../Img/team/team2.png" />
+               <div class="content_slide mt-3">
+                   <span>Patientenleitstelle</span><br>
+                   <span>Koordination</span><br>
+                   <span class="color_blue">Nadine Buchczik</span><br>
+                   <span>nadine.buchczik@wzat.de</span><br>
+                   <span>Telefon +49 2306 7655–222</span>
+               </div>
+
+           </div>
+           <div>
+               <img src="../../Img/team/team3.png" />
+               <div class="content_slide mt-3">
+                   <span>Technik</span><br>
+                   <span>Koordination</span><br>
+                   <span class="color_blue">Jonas Lammers</span><br>
+                   <span>jonas.lammers@wzat.de</span><br>
+                   <span>Telefon +49 2306 7655–202</span><br>
+               </div>
+
+           </div>
+           <div>
+               <img src="../../Img/team/team4.png" />
+                    <div class="content_slide mt-3">
+                        <span>Patientenleitstelle</span><br>
+                        <span>Koordination</span><br>
+                        <span class="color_blue">Melanie Bitter</span><br>
+                        <span>melanie.bitter@wzat.de</span><br>
+                        <span>Telefon +49 2306 7655–22</span>2<br>
+                    </div>
+
+           </div>
+           <div>
+               <img src="../../Img/team/team1.png" />
+               <div class="content_slide mt-3">
+                   <span>Medizinische Leitun</span>g<br>
+                   <span class="color_blue">Prof. Dr. med.</span><br>
+                   <span class="color_blue">Christian Perings</span>
+               </div>
+           </div>
 
        </div>
 
