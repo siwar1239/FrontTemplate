0960273566bc1f1acc71ef1072e28d48c4a6fb24
siwar zayani <siwar.zayani@softtodo.com>
2020-04-17 11:47:54 +0200

    adit style navbar header open...

--- Templates/Partials/header.php
+++ Templates/Partials/header.php
@@ -3,19 +3,19 @@
         <div class="container">
             <div class="row">
                 <div class="header-top-cols w-100">
-                    <div class="col-lg-4 col-md-4 w-100 button-left">
+                    <div class="col-lg-5 col-md-5 w-100 button-left">
                         <a href="#" class="btn-Zuweiser">Zuweiser</a>
                     </div>
-                    <div class="col-lg-8 col-md-8 w-100 menu-top-right">
+                    <div class="col-lg-7 col-md-7 w-100 menu-top-right">
                         <div class="row">
                             <div class="m-auto tel">+49 2306 7655-0</div>
                             <div class="m-auto"> Datenschutz</div>
                             <div class="search-form m-auto">
-                                    <form action="#" method="get">
-                                        <span class="icon-search"></span>
-                                        <input type="text" name="q" placeholder="Suchfunktion"/>
-                                    </form>
-                             </div>
+                                <form action="#" method="get">
+                                    <span class="icon-search"></span>
+                                    <input type="text" name="q" placeholder="Suchfunktion"/>
+                                </form>
+                            </div>
                             <div class="m-auto">Schriftgröße AA</div>
                             <div class="ml-auto">ENG</div>
                         </div>
@@ -31,10 +31,10 @@
         <div class="row">
             <div class="col-lg-3 col-md-3 col-sm-3 logo">
                 <a href="home.php" class="logo-a" title="logo">
-                        <img src="../../Img/logo_header.jpg" class="py-3" >
+                    <img src="../../Img/logo_header.jpg" class="py-3" >
                 </a>
             </div>
-            <div class="col-sm-9 col-md-9  col-lg-9 menu-right">
+            <div class="col-lg-9 col-md-9  col-sm-9 menu-right">
                 <div class="mobile-nav-opener">
                     <div id="nav-icon">
                         <span></span>
@@ -47,16 +47,8 @@
                     <div class="main-menu-navs">
                         <div class="main-nav">
                             <nav id="main-navigation">
-                                <ul class="first-level-menu">
+                                <ul class="ul-menu">
                                     <li class=""><a href="#">Unsere Versorgungsprogramme</a></li>
-                                    <li class="has-dropdown"><a
-                                                href="#">ssssss <span class="open-mobile-menu"></span></a>
-                                        <ul class="dropdown-menu">
-                                            <li><a href="#" title="title1" >title1</a></li>
-                                            <li><a href="#" title="title2" >title2</a></li>
-
-                                        </ul>
-                                    </li>
                                     <li class=""><a href="#">Anwenderberichte</a></li>
                                     <li class=""><a href="#">Über das WZAT</a></li>
                                     <li class=""><a href="#">Kontakt</a></li>
@@ -68,6 +60,6 @@
                     </div>
                 </div>
             </div>
-    </div>
+        </div>
     </div>
 </header>
